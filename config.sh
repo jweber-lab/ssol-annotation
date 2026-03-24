fadba4456452069b5fc39e1f3e3e672ab5bdb670
Christopher McAllester <cmcallester@gmail.com>
2026-03-24 11:53:08 -0500

    partial config update for protein fa files

--- config.sh
+++ config.sh
@@ -33,18 +33,37 @@ TSA_FASTA="${REPO_ROOT}/../transcriptomes/hebert-2016/PRJNA304161/GEEE01/GEEE01.
 TSA_PROTEINS="${REPO_ROOT}/../transcriptomes/hebert-2016/PRJNA304161/GEEE01/GEEE01.1.fsa_aa.gz"
 
 # ── Reference species (proteomes & CDS for training) ────────────────────────
-GENOMES_ROOT="${REPO_ROOT}/../genomes/downloads"
-REF_LABELS=(lint spro dlat seri hmic egra)
 
-REF_PROTEINS=()
-for lbl in "${REF_LABELS[@]}"; do
-    REF_PROTEINS+=("${GENOMES_ROOT}/${lbl}/protein.faa")
-done
+REF_PROTEINS=(
+    "${REPO_ROOT}/../genomes/downloads/spro/ncbi_dataset/data/GCA_053814205.1/protein.faa"
+    "${REPO_ROOT}/../genomes/downloads/dlat/wormbase_parasite/dibothriocephalus_latus.PRJEB1206.WBPS19.protein.fa"
+    "${REPO_ROOT}/../genomes/downloads/seri/wormbase_parasite/spirometra_erinaceieuropaei.PRJEB1202.WBPS19.protein.fa"
+    "${REPO_ROOT}/../genomes/downloads/hmic/wormbase_parasite/hymenolepis_microstoma.PRJEB124.WBPS19.protein.fa"
+    "${REPO_ROOT}/../genomes/downloads/egra/wormbase_parasite/echinococcus_granulosus.PRJNA754835.WBPS19.protein.fa"
+)
+REF_CDS=(
+    "${REPO_ROOT}/../genomes/downloads/spro/ncbi_dataset/data/GCA_053814205.1/cds_from_genomic.fna"
+    "${REPO_ROOT}/../genomes/downloads/dlat/ncbi_dataset/data/GCA_900617775.1/cds_from_genomic.fna"
+    "${REPO_ROOT}/../genomes/downloads/seri/ncbi_dataset/data/GCA_000951995.1/cds_from_genomic.fna"
+    "${REPO_ROOT}/../genomes/downloads/hmic/ncbi_dataset/data/GCA_000469805.3/cds_from_genomic.fna"
+    "${REPO_ROOT}/../genomes/downloads/egra/ncbi_dataset/data/GCA_021556725.1/cds_from_genomic.fna"
+)
 
-REF_CDS=()
-for lbl in "${REF_LABELS[@]}"; do
-    REF_CDS+=("${GENOMES_ROOT}/${lbl}/cds_from_genomic.fna")
-done
+
+
+# GENOMES_ROOT="${REPO_ROOT}/../genomes/downloads"
+# REF_LABELS=(lint spro dlat seri hmic egra)
+# ASSEMBLY_IDS=(GCA_036362985.1 GCA_053814205.1 GCA_900617775.1 GCA_000951995.1 GCA_000469805.3 GCA_021556725.1)
+
+# REF_PROTEINS=()
+# for i in "${!REF_LABELS[@]}"; do
+#     REF_PROTEINS+=("${GENOMES_ROOT}/${REF_LABELS[${i}]}/ncbi_dataset/data/${ASSEMBLY_IDS[${i}]}/protein.faa")
+# done
+
+# REF_CDS=()
+# for i in "${!REF_LABELS[@]}"; do
+#     REF_CDS+=("${GENOMES_ROOT}/${REF_LABELS[${i}]}/ncbi_dataset/data/${ASSEMBLY_IDS[${i}]}/cds_from_genomic.fna")
+# done
 
 # ── UniProt / Swiss-Prot ─────────────────────────────────────────────────────
 UNIPROT_FASTA="/path/to/uniprot_sprot.fasta"       # UPDATE: Swiss-Prot FASTA
